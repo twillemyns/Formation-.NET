19c7b0f3be70222252f57a9b0de913cd0a1ad6fb
Théo Willemyns <willemynstheo@gmail.com>
2025-03-06 12:33:50 +0100

    Réajout du css

--- 16-React/01-Répertoire/repertoire/src/Components/Table.jsx
+++ 16-React/01-Répertoire/repertoire/src/Components/Table.jsx
@@ -25,7 +25,7 @@ const Table = () => {
 
     return (
         <>
-            <table>
+            <table className="table table-dark">
                 <thead>
                     <tr>
                         <th>#</th>
